8deca49cb03ded18d5dd82c11fb7fbfc371f4547
Addiel Juarez <addieljuarez@gmail.com>
2022-08-08 23:14:11 -0500

    git ignore

--- Ebook_IntroduccionANode/greeting/index.js
+++ Ebook_IntroduccionANode/greeting/index.js
@@ -1,15 +1,17 @@
+const colors = require('colors/safe');
+
 const args = process.argv.slice(2);
 
-const [name = "Friend"] = args;
+const [name = 'Friend'] = args;
 
 const hour = new Date().getHours();
 
 if (hour >= 6 && hour < 12) {
-    console.log(`Good morning ${name}`);
+  console.log(colors.yellow(`Good morning ${name}`));
 } else if (hour >= 12 && hour < 18) {
-    console.log(`Good afternoon ${name}`);
+  console.log(colors.green(`Good afternoon ${name}`));
 } else if (hour >= 18 && hour < 23) {
-    console.log(`Good evening ${name}`);
+  console.log(colors.cyan(`Good evening ${name}`));
 } else {
-    console.log(`Good night ${name}`);
+  console.log(colors.blue(`Good night ${name}`));
 }
