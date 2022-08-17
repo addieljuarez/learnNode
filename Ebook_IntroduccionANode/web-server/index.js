afd888efdb53e3e7835f40a00a84c78500734bca
Addiel Juarez <addieljuarez@gmail.com>
2022-08-17 11:44:41 -0500

    web-server

---
+++ Ebook_IntroduccionANode/web-server/index.js
@@ -0,0 +1,50 @@
+
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const pug = require('pug');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    
+    const pathurl = url.parse(req.url, true);
+    const filename = path.basename(pathurl.pathname, '.html');
+    const filepath = path.join(__dirname, 'views', `${filename}.pug`);
+
+    fs.readFile(filepath, 'utf-8', (err, file) => {
+
+        if(err){
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain');
+            res.write('404 Not Found');
+            res.end();
+            return;
+        }else{
+            res.statusCode = 200;
+            res.setHeader('Contebt-Type', 'text/html');
+            const html = pug.render(file, {
+                pageTitle: 'Our great Website',
+                menu: [
+                    {
+                        name: 'Home',
+                        link: 'index.html'
+                    },
+                    {
+                        name: 'About',
+                        link: 'about.html'
+                    }
+                ]
+            });
+            // res.write(html);
+            res.end(html);
+        }
+        
+    });
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
